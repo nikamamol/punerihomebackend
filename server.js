974179a3b4c9e57e1c285ba1a67d766d55aaca0e
amol9145 <amol@koinetmedia.com>
2026-02-14 18:02:41 +0530

    na

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const viewingRoutes = require('./routes/viewingRoutes');
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://mypunerihomes.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
